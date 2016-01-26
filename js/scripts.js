303c4939a99f6115dd57363c08c5d251850bfc21
Christine Tacha and Ramon de la Cruz <tacha.christine@gmail.com>
2016-01-25 16:09:50 -0800

    passed first spec

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+function Todo(task) {
+  this.task = task;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,6 @@
-describe("toDoList", function() {
+describe("Todo", function() {
   it("will add a task to the user's to do list", function() {
-    var itemList = new Todo("vacuum");
-    expect(itemList).to.equal("vacuum")
+    var itemList = new Todo("dust");
+    expect(itemList.task).to.equal("dust");
   });
 });
